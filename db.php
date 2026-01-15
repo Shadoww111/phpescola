dcdd380fc22927bbb8ad13cb08c657467fd5b00b
Shadow <pedro.shadow.costa@gmail.com>
2026-01-15 13:27:25 +0000

    Create db.php

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost"; // Nome do servidor, geralmente localhost
+$username = "root"; // Usuário padrão do MySQL no XAMPP
+$password = ""; // Senha do MySQL no XAMPP, normalmente em branco
+$dbname = "psi"; // Nome do banco de dados que você criou
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+// Checa a conexão
+if ($conn->connect_error) {
+    die("A conexão falhou: " . $conn->connect_error);
+}
+
+echo "Conexão bem-sucedida!";
+
+$pergunta="Insert clientes values('".$nome."','".$idade."')";
+$resultado= mysqli_query($conn, $pergunta);
+
+?>
